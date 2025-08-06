d7a534a408f0308b0cdefdd066943ff52f74205d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-08-06 12:35:19 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -520,6 +520,30 @@ int main() {
     else
         cout << "NO" << endl;
 }
+#include <iostream>
+https://codeforces.com/problemset/problem/69/A
+// A. Young Physicist
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int xSum = 0, ySum = 0, zSum = 0;
+    for (int i = 0; i < n; ++i) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        xSum += x;
+        ySum += y;
+        zSum += z;
+    }
+    if (xSum == 0 && ySum == 0 && zSum == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
+
 using namespace std;
 // A. Way Too Long Words
 // problemset/problem/71/A
