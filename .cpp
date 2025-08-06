45b38dcfa595faf3741ade8ea6d7dafc5b389bb6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-08-06 18:56:34 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -264,6 +264,40 @@ int main(){
     }
     cout << cnt * 2;
 }
+https://codeforces.com/problemset/problem/32/A
+// A. Reconnaissance
+using namespace std;
+int main(){
+    int n, d, height[1000]; cin >> n >> d;
+    for (int i = 0; i < n; ++i) cin >> height[i];
+    sort(height, height + n);
+    int i1 = 0, i2 = 1, ways = 0;
+    while (i2 < n){
+        if (height[i2] - height[i1] <= d){
+            ways += (i2 - i1);
+            ++i2;
+        }
+        else    ++i1;
+    }
+    ways *= 2;
+    cout << ways;
+}
+using namespace std;
+int main() {
+    int n, d; cin >> n >> d;
+    vector<int> height(n);
+    for (int& h : height)
+        cin >> h;
+    sort(height.begin(), height.end());
+    int i1 = 0, i2 = 1, ways = 0;
+    while (i2 < n) {
+        if (height[i2] - height[i1] <= d) {
+            ways += (i2 - i1);
+            ++i2;
+        } else    ++i1;
+    }
+    cout << (ways * 2) << '\n';
+}
 using namespace std;
 http://codeforces.com/problemset/problem/32/B
 // B. Borze
